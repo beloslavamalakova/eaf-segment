eb4fb866ebc4fd2ac42655fa4ffb70b7c031c792
beloslavamalakova <beloslava.malakova@gmail.com>
2025-09-08 11:42:15 +0200

    initial setup

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name="Segment Objects in Images",       # Title of your project
+    version="0.1.0",
+    author="Beloslava Malakova, Boris Gachevski, Nea, Dion",  # Just names
+    description="short description",
+)
